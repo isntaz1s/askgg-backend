1caac72ce233ad472bf99842900e8811b5a3cd37
isntaz1s <azis190407@gmail.com>
2024-11-15 11:04:50 +0700

    feat(cors): add cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cors({ origin: '*', credentials: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
